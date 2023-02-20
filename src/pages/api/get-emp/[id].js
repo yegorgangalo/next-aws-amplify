9477a94e888eb88ac62ab7b4bd679ee7605613cd
yegorgangalo <gangaloyegor@gmail.com>
2023-02-20 13:17:28 +0200

    add redis cache for localhost

--- src/pages/api/get-emp/[id].js
+++ src/pages/api/get-emp/[id].js
@@ -1,24 +1,36 @@
+// import Redis from 'ioredis'
 import { fetcher, sendResponse } from '../common'
 
 const { BASE_SHOP_URL } = process.env
 
-export const fetchEmployerDataHandler = (req, res) => {
-  const empId = req.query.id
-  const url = `${BASE_SHOP_URL}drGetEmployer/?emp_id=${empId}`
-  console.log('fetchEmployerDataHandler', { BASE_SHOP_URL, url })// cloudwatch
+export const fetchEmployerDataHandler = async (req, res) => {
+  try {
+    const empId = req.query.id
+    const url = `${BASE_SHOP_URL}drGetEmployer/?emp_id=${empId}`
 
-  return fetcher(url)
-    .then((apiDate) => {
-      console.log('fetcher result:', apiDate);
-      if (apiDate.errno) {
-        throw Error(apiDate.message || 'Server error')
-      }
-      return sendResponse({ data: { apiDate }, res })
-    })
-    .catch((err) => {
-      console.log('fetcher catch err:', err);
-      return sendResponse({ data: { err: err.message }, status: 500, res })
-    })
+    // const redis = new Redis({
+    //   port: 6379,
+    //   host: "127.0.0.1",
+    // })
+
+    // const cache = await redis.get(url)
+    // console.log('REDIS cache=', { url, cache })//cloudwatch
+    // if (cache) {
+    //   console.log('load from cache', cache);
+    //   return sendResponse({ data: { apiDate: JSON.parse(cache), isCache: true }, res })
+    // }
+
+    const apiDate = await fetcher(url)
+    console.log('fetcher result:', apiDate);
+    if (apiDate.errno) {
+      throw Error(apiDate.message || 'Server error')
+    }
+    redis.set(url, JSON.stringify(apiDate))
+    return sendResponse({ data: { apiDate }, res })
+  } catch (error) {
+    console.log('fetcher catch err:', err);
+    return sendResponse({ data: { err: err.message }, status: 500, res })
+  }
 }
 
 export default function employerController(req, res) {
