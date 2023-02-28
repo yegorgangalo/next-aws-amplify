42b8778522fb3facdc9f1306f96111501e7d8ee2
yegorgangalo <gangaloyegor@gmail.com>
2023-02-28 20:35:55 +0200

    add beauty json show

--- src/pages/api/get-emp/[id].js
+++ src/pages/api/get-emp/[id].js
@@ -1,4 +1,4 @@
-// import Redis from 'ioredis'
+import Redis from 'ioredis'
 import { fetcher, sendResponse } from '../common'
 
 const { BASE_SHOP_URL } = process.env
@@ -8,17 +8,17 @@ export const fetchEmployerDataHandler = async (req, res) => {
     const empId = req.query.id
     const url = `${BASE_SHOP_URL}drGetEmployer/?emp_id=${empId}`
 
-    // const redis = new Redis({
-    //   port: 6379,
-    //   host: "127.0.0.1",
-    // })
+    const redis = new Redis({
+      port: 6379,
+      host: "127.0.0.1",
+    })
 
-    // const cache = await redis.get(url)
-    // console.log('REDIS cache=', { url, cache })//cloudwatch
-    // if (cache) {
-    //   console.log('load from cache', cache);
-    //   return sendResponse({ data: { apiDate: JSON.parse(cache), isCache: true }, res })
-    // }
+    const cache = await redis.get(url)
+    console.log('REDIS cache=', { url, cache })//cloudwatch
+    if (cache) {
+      console.log('load from cache', cache);
+      return sendResponse({ data: { apiDate: JSON.parse(cache), isCache: true }, res })
+    }
 
     const apiDate = await fetcher(url)
     console.log('fetcher result:', apiDate);
@@ -28,8 +28,8 @@ export const fetchEmployerDataHandler = async (req, res) => {
     redis.set(url, JSON.stringify(apiDate))
     return sendResponse({ data: { apiDate }, res })
   } catch (error) {
-    console.log('fetcher catch err:', err);
-    return sendResponse({ data: { err: err.message }, status: 500, res })
+    console.log('fetcher catch err:', error);
+    return sendResponse({ data: { error: error.message }, status: 500, res })
   }
 }
 
